eb28ad388e0eb4fe5b56f23fd16fe21306b60b94
KennFischerAmfitech <kf@amfitech.dk>
2026-05-15 11:53:53 +0200

    Renamed, and removed old way of doing things.

--- Amfitrack.cpp
+++ Amfitrack.cpp
@@ -1,346 +1,213 @@
-#include "project_conf.h"
-#include "src/project_conf.h"
-#include "lib_AmfiProt_API.hpp"
-#include "Amfitrack.hpp"
+//-----------------------------------------------------------------------------
+//
+//                              AMFITECH APS
+//
+//                          ALL RIGHTS RESERVED
+//
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Section: Includes
+//-----------------------------------------------------------------------------
+
+#include "Amfitrack.h"
+
+#include "lib/amfiprotapi/lib_AmfiProt_API.hpp"
 #include "lib/lib_log/lib_log.h"
+#include "src/Amfitrack_Devices.h"
+#include "src/Amfitrack_task.h"
+#include "src/HID_Monitor.h"
+#include "src/project_conf.h"
 
+#include <memory>
+#include <utility>
+
+//-----------------------------------------------------------------------------
+// Section: Define
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+// Section: Typedef
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+// Section: Macro
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+// Section: Variables
+//-----------------------------------------------------------------------------
+static AmfiProt_API *amfiprot_api = nullptr;
+static std::unique_ptr<HIDMonitor> hid_monitor = nullptr;
+volatile static bool stop_running = false;
+//-----------------------------------------------------------------------------
+// Section: Function prototypes
+//-----------------------------------------------------------------------------
+//-----------------------------------------------------------------------------
+// Section: Functions
+//-----------------------------------------------------------------------------
+static void _run_all_amfitrack()
+{
 #ifdef USE_USB
-#include "HID_Monitor.h"
+	hid_monitor->run();
 #endif
-#ifdef USE_THREAD_BASED
-#include <iostream>
-#include <thread>
-#endif
-
-// #define AMFITRACK_DEBUG_INFO
-
-static bool stop_running = false;
-
-AMFITRACK::AMFITRACK()
-{
-	// Initialize Name with null characters
-	for (int i = 0; i < MAX_NUMBER_OF_DEVICES; i++)
-	{
-		memset(Name[i], 0, MAX_NAME_LENGTH);
-		DeviceActive[i] = false;
-		Position[i].position_x_in_m = 0;
-		Position[i].position_y_in_m = 0;
-		Position[i].position_z_in_m = 0;
-
-		Position[i].orientation_x = 0;
-		Position[i].orientation_y = 0;
-		Position[i].orientation_z = 0;
-		Position[i].orientation_w = 0;
-	}
+	amfiprot_api->amfiprot_run();
+	amfitrack_task::run();
 }
 
-AMFITRACK::~AMFITRACK()
-{
-}
-
-void AMFITRACK::background_amfitrack_task(AMFITRACK *inst)
+void AMFITRACK_NEW::background_amfitrack_task(AMFITRACK_NEW *inst)
 {
 	(void)inst;
-#if defined(USE_THREAD_BASED)
-#ifdef USE_USB
-	AmfiProt_API &api = AmfiProt_API::getInstance();
-	HIDMonitorCallbacks cb;
-
-	cb.txPoll = [&api](size_t &queueIdx, size_t &len, uint8_t &txId, void *&data) -> bool
-	{
-		return api.isDataReadyForTransmit(&queueIdx, &len, &txId, &data);
-	};
-
-	cb.txDone = [&api](uint8_t queueIdx)
-	{
-		api.set_transmit_ongoing_and_check_respons_request(queueIdx);
-	};
-
-	cb.rxPush = [&api](const uint8_t *data, size_t len)
-	{
-		// #if defined(_WIN32) || defined(__linux__) || defined(__APPLE__)
-		//     api.deserialize_frame(data, (uint8_t)len, std::chrono::steady_clock::now());
-		// #else
-		api.deserialize_frame(data, (uint8_t)len);
-		// #endif
-	};
-
-	HIDMonitor monitor(std::move(cb));
-	monitor.init();
-#endif
-	AmfiProt_API &amfiprot_api = AmfiProt_API::getInstance();
-	AMFITRACK &AMFITRACK = AMFITRACK::getInstance();
-
-	LOG_D("Background thread started!");
-
 	while (!stop_running)
 	{
-#ifdef USE_USB
-		monitor.run();
-#endif
-
-		amfiprot_api.amfiprot_run();
-
-		for (uint8_t devices = 0; devices < MAX_NUMBER_OF_DEVICES; devices++)
-		{
-			if (AMFITRACK.getDeviceActive(devices))
-			{
-				AMFITRACK.checkDeviceDisconnected(devices);
-			}
-		}
+		_run_all_amfitrack();
 
 		std::this_thread::sleep_for(std::chrono::milliseconds(1));
 	}
-#endif
 }
 
-void AMFITRACK::start_amfitrack_task(void)
+AMFITRACK_NEW &AMFITRACK_NEW::getInstance()
 {
-#if defined(USE_THREAD_BASED)
+	static AMFITRACK_NEW instance;
+	return instance;
+}
+
+void AMFITRACK_NEW::init()
+{
+	amfiprot_api = &AmfiProt_API::getInstance();
+	AMFITRACK_Devices::getInstance().reset_devices();
+#ifdef USE_USB
+	if (!hid_monitor)
+	{
+		HIDMonitorCallbacks cb;
+
+		cb.txPoll = [](size_t &queueIdx, size_t &len, uint8_t &txId, void *&data) -> bool
+		{
+			return amfiprot_api->isDataReadyForTransmit(&queueIdx, &len, &txId, &data);
+		};
+
+		cb.txDone = [](uint8_t queueIdx)
+		{
+			amfiprot_api->set_transmit_ongoing_and_check_respons_request(queueIdx);
+		};
+
+		cb.rxPush = [](const uint8_t *data, size_t len)
+		{
+			amfiprot_api->deserialize_frame(data, static_cast<uint8_t>(len));
+		};
+
+		hid_monitor = std::make_unique<HIDMonitor>(std::move(cb));
+		hid_monitor->init();
+	}
+#endif
+	amfitrack_task::init();
+}
+
+void AMFITRACK_NEW::start_task()
+{
 	stop_running = false;
-
-	LOG_D("Starting Background thread!");
-
 	// Create a thread object
 	std::thread background_thread(background_amfitrack_task, this);
 
 	background_thread.detach();
-#endif
 }
 
-void AMFITRACK::stop_amfitrack_task(void)
+void AMFITRACK_NEW::stop_task()
 {
 	stop_running = true;
 }
 
-void AMFITRACK::amfitrack_main_loop(void)
+void AMFITRACK_NEW::run()
 {
-#ifdef USE_USB
-	AmfiProt_API &api = AmfiProt_API::getInstance();
-	HIDMonitorCallbacks cb;
-
-	cb.txPoll = [&api](size_t &queueIdx, size_t &len, uint8_t &txId, void *&data) -> bool
-	{
-		return api.isDataReadyForTransmit(&queueIdx, &len, &txId, &data);
-	};
-
-	cb.txDone = [&api](uint8_t queueIdx)
-	{
-		api.set_transmit_ongoing_and_check_respons_request(queueIdx);
-	};
-
-	cb.rxPush = [&api](const uint8_t *data, size_t len)
-	{
-		// #if defined(_WIN32) || defined(__linux__) || defined(__APPLE__)
-		//     api.deserialize_frame(data, (uint8_t)len, std::chrono::steady_clock::now());
-		// #else
-		api.deserialize_frame(data, (uint8_t)len);
-		// #endif
-	};
-
-	HIDMonitor monitor(std::move(cb));
-	monitor.init();
-#endif
-	AmfiProt_API &amfiprot_api = AmfiProt_API::getInstance();
-	AMFITRACK &AMFITRACK = AMFITRACK::getInstance();
-
-#ifdef USE_USB
-	monitor.run();
-#endif
-	amfiprot_api.amfiprot_run();
-
-#ifdef USE_ACTIVE_DEVICE_HANDLING
-	for (uint8_t devices = 0; devices < MAX_NUMBER_OF_DEVICES; devices++)
-	{
-		if (AMFITRACK.getDeviceActive(devices))
-		{
-			AMFITRACK.checkDeviceDisconnected(devices);
-		}
-	}
-#endif
+	_run_all_amfitrack();
 }
 
-void AMFITRACK::initialize_amfitrack()
+void AMFITRACK_NEW::reset_devices()
 {
-#ifdef USE_USB
-	AmfiProt_API &api = AmfiProt_API::getInstance();
-	HIDMonitorCallbacks cb;
-
-	cb.txPoll = [&api](size_t &queueIdx, size_t &len, uint8_t &txId, void *&data) -> bool
-	{
-		return api.isDataReadyForTransmit(&queueIdx, &len, &txId, &data);
-	};
-
-	cb.txDone = [&api](uint8_t queueIdx)
-	{
-		api.set_transmit_ongoing_and_check_respons_request(queueIdx);
-	};
-
-	cb.rxPush = [&api](const uint8_t *data, size_t len)
-	{
-		api.deserialize_frame(data, (uint8_t)len);
-	};
-
-	HIDMonitor monitor(std::move(cb));
-	monitor.init();
-#endif
+	AMFITRACK_Devices::getInstance().reset_devices();
 }
 
-void AMFITRACK::setDeviceName(uint8_t DeviceID, char *name, uint8_t length)
+bool AMFITRACK_NEW::get_sensor(uint8_t device_id, AMFITRACK_Sensor *sensor) const
 {
-	// Check for valid device ID and name length
-	if (length >= MAX_NAME_LENGTH)
-		return;
-#ifdef USE_THREAD_BASED
-	const std::lock_guard<std::mutex> lock(mutName);
-#endif // USE_THREAD_BASED
-	for (uint8_t i = 0; i < length; i++)
-	{
-		Name[DeviceID][i] = name[i];
-	}
-	Name[DeviceID][length] = '\0'; // Ensure null termination
-
-	LOG_D(Name[DeviceID]);
+	return AMFITRACK_Devices::getInstance().get_sensor(device_id, sensor);
 }
 
-void AMFITRACK::setConfiguration(uint8_t DeviceID, uint32_t UID, lib_Generic_Parameter_Value_t parameter)
+bool reset_sensor(uint8_t device_id)
 {
-	AmfiProt_API &amfiprot_api = AmfiProt_API::getInstance();
-	lib_AmfiProt_ConfigValueUID_t ConfigurationPayload = {};
-	ConfigurationPayload.payloadID = lib_AmfiProt_PayloadID_SetConfigurationValueUID;
-	ConfigurationPayload.uid = UID;
-	ConfigurationPayload.value = parameter;
-	uint8_t payloadSize = sizeof(ConfigurationPayload) - sizeof(ConfigurationPayload.value) + lib_Generic_Parameter_SizeWithType(ConfigurationPayload.value);
-
-	amfiprot_api.queue_frame(&ConfigurationPayload, payloadSize, libAmfiProt_PayloadType_Common, lib_AmfiProt_packetType_NoAck, DeviceID);
+	return AMFITRACK_Devices::getInstance().reset_sensor(device_id);
 }
 
-// This function checks if the device is disconnected,
-// sets the DeviceActive variable if it has
-// and returns true if it has been disconnected, false if it is still active.
-bool AMFITRACK::checkDeviceDisconnected(uint8_t DeviceID)
+bool AMFITRACK_NEW::get_source(uint8_t device_id, AMFITRACK_Source *source) const
 {
-#ifdef USE_ACTIVE_DEVICE_HANDLING
-	time_t CurrentTime = time(0);
-
-	if (difftime(CurrentTime, DeviceLastTimeSeen[DeviceID]) > 5.0)
-	{
-#ifdef USE_THREAD_BASED
-		const std::lock_guard<std::mutex> lock(mutDeviceActive);
-#endif // USE_THREAD_BASED
-		DeviceActive[DeviceID] = false;
-		LOG_I("Device %u disconnected", DeviceID);
-		return true;
-	}
-#endif
-	return false;
+	return AMFITRACK_Devices::getInstance().get_source(device_id, source);
 }
 
-void AMFITRACK::setDeviceActive(uint8_t DeviceID)
+bool reset_source(uint8_t device_id)
 {
-#ifdef USE_ACTIVE_DEVICE_HANDLING
-#ifdef USE_THREAD_BASED
-	const std::lock_guard<std::mutex> lock(mutDeviceActive);
-#endif // USE_THREAD_BASED
-	if (!DeviceActive[DeviceID])
-		LOG_I("Device %u connected", DeviceID);
-	DeviceActive[DeviceID] = true;
-	DeviceLastTimeSeen[DeviceID] = time(0);
-	LOG_D("Device is active", DeviceID);
-
-#endif
+	return AMFITRACK_Devices::getInstance().reset_source(device_id);
 }
 
-bool AMFITRACK::getDeviceActive(uint8_t DeviceID)
+//-----------------------------------------------------------------------------
+// Old function will be deprecated
+//-----------------------------------------------------------------------------
+void AMFITRACK_NEW::initialize_amfitrack()
 {
-#ifdef USE_THREAD_BASED
-	const std::lock_guard<std::mutex> lock(mutDeviceActive);
-#endif // USE_THREAD_BASED
-	return DeviceActive[DeviceID];
+	init();
+}
+void AMFITRACK_NEW::start_amfitrack_task(void)
+{
+	start_task();
+}
+void AMFITRACK_NEW::stop_amfitrack_task(void)
+{
+	stop_task();
+}
+void AMFITRACK_NEW::amfitrack_main_loop(void)
+{
+	run();
 }
 
-void AMFITRACK::setDevicePose(uint8_t DeviceID, lib_AmfiProt_Amfitrack_Pose_t Pose)
+bool AMFITRACK_NEW::getDeviceActive(uint8_t DeviceID)
 {
-#ifdef USE_THREAD_BASED
-	const std::lock_guard<std::mutex> lock(mutPosition);
-#endif // USE_THREAD_BASED
-	memcpy(&Position[DeviceID], &Pose, sizeof(lib_AmfiProt_Amfitrack_Pose_t));
+	AMFITRACK_Sensor sensor;
+	AMFITRACK_Devices::getInstance().get_sensor(DeviceID, &sensor);
+	return sensor.active;
 }
-
-void AMFITRACK::getDevicePose(uint8_t DeviceID, lib_AmfiProt_Amfitrack_Pose_t *Pose)
+void AMFITRACK_NEW::getDevicePose(uint8_t DeviceID, lib_AmfiProt_Amfitrack_Pose_t *Pose)
 {
-	if (!getDeviceActive(DeviceID))
-		return;
-#ifdef USE_THREAD_BASED
-	const std::lock_guard<std::mutex> lock(mutPosition);
-#endif // USE_THREAD_BASED
-	memcpy(Pose, &Position[DeviceID], sizeof(lib_AmfiProt_Amfitrack_Pose_t));
+	AMFITRACK_Sensor sensor;
+	AMFITRACK_Devices::getInstance().get_sensor(DeviceID, &sensor);
+	Pose->position_x_in_m = sensor.pose.Position_X;
+	Pose->position_y_in_m = sensor.pose.Position_Y;
+	Pose->position_z_in_m = sensor.pose.Position_Z;
+
+	Pose->orientation_x = sensor.pose.Orientation_X;
+	Pose->orientation_y = sensor.pose.Orientation_Y;
+	Pose->orientation_z = sensor.pose.Orientation_Z;
+	Pose->orientation_w = sensor.pose.Orientation_W;
 }
-
-void AMFITRACK::setDeviceIMU(uint8_t DeviceID, lib_AmfiProt_Amfitrack_IMU_t imuData)
+void AMFITRACK_NEW::getDeviceIMU(uint8_t DeviceID, lib_AmfiProt_Amfitrack_IMU_t *imuData)
 {
-#ifdef USE_THREAD_BASED
-	const std::lock_guard<std::mutex> lock(mutIMU);
-#endif // USE_THREAD_BASED
-	memcpy(&IMUData[DeviceID], &imuData, sizeof(lib_AmfiProt_Amfitrack_IMU_t));
+	AMFITRACK_Sensor sensor;
+	AMFITRACK_Devices::getInstance().get_sensor(DeviceID, &sensor);
+	imuData->acceleration_x_in_mg = sensor.imu.Acceleration_X / 1000.0f;
+	imuData->acceleration_x_in_mg = sensor.imu.Acceleration_X / 1000.0f;
+	imuData->acceleration_x_in_mg = sensor.imu.Acceleration_X / 1000.0f;
+
+	imuData->rotation_x_in_rad_per_sec = sensor.imu.Rotation_X;
+	imuData->rotation_y_in_rad_per_sec = sensor.imu.Rotation_Y;
+	imuData->rotation_z_in_rad_per_sec = sensor.imu.Rotation_Z;
 }
-
-void AMFITRACK::getDeviceIMU(uint8_t DeviceID, lib_AmfiProt_Amfitrack_IMU_t *imuData)
+void AMFITRACK_NEW::getSensorMeasurements(uint8_t DeviceID, lib_AmfiProt_Amfitrack_Sensor_Measurement_t *SensorMeasurement)
 {
-	if (!getDeviceActive(DeviceID))
-		return;
-#ifdef USE_THREAD_BASED
-	const std::lock_guard<std::mutex> lock(mutSensorMeasurements);
-#endif // USE_THREAD_BASED
-	memcpy(imuData, &IMUData[DeviceID], sizeof(lib_AmfiProt_Amfitrack_IMU_t));
-}
-
-void AMFITRACK::setSensorMeasurements(uint8_t DeviceID, lib_AmfiProt_Amfitrack_Sensor_Measurement_t SensorMeasurement)
-{
-#ifdef USE_THREAD_BASED
-	const std::lock_guard<std::mutex> lock(mutSensorMeasurements);
-#endif // USE_THREAD_BASED
-	memcpy(&SensorMeasurements[DeviceID], &SensorMeasurement, sizeof(lib_AmfiProt_Amfitrack_Sensor_Measurement_t));
-}
-
-void AMFITRACK::getSensorMeasurements(uint8_t DeviceID, lib_AmfiProt_Amfitrack_Sensor_Measurement_t *SensorMeasurement)
-{
-	if (!getDeviceActive(DeviceID))
-		return;
-#ifdef USE_THREAD_BASED
-	const std::lock_guard<std::mutex> lock(mutSensorMeasurements);
-#endif // USE_THREAD_BASED
-	memcpy(SensorMeasurement, &SensorMeasurements[DeviceID], sizeof(lib_AmfiProt_Amfitrack_Sensor_Measurement_t));
+	AMFITRACK_Sensor sensor;
+	AMFITRACK_Devices::getInstance().get_sensor(DeviceID, &sensor);
+	SensorMeasurement = &sensor.sensorMeasurement;
 }
 
 #if defined(_WIN32) || defined(__linux__) || defined(__APPLE__)
-std::chrono::steady_clock::time_point getTimestampMicroseconds()
+void AMFITRACK_NEW::getSensorTimestamp(uint8_t DeviceID, std::chrono::steady_clock::time_point *time_stamp)
 {
-	std::chrono::steady_clock::time_point time;
-#if defined(_WIN32) || defined(__linux__) || defined(__APPLE__)
-	time = std::chrono::steady_clock::now();
-#else
-	// Unknown OS
-#endif
-	return time;
+	AMFITRACK_Sensor sensor;
+	AMFITRACK_Devices::getInstance().get_sensor(DeviceID, &sensor);
+	time_stamp = &sensor.timestamp;
 }
-
-void AMFITRACK::setSensorTimestamp(uint8_t DeviceID, std::chrono::steady_clock::time_point time_stamp)
-{
-#ifdef USE_THREAD_BASED
-	const std::lock_guard<std::mutex> lock(mutSensorTimestamps);
-#endif // USE_THREAD_BASED
-	memcpy(&SensorTimestamps[DeviceID], &time_stamp, sizeof(std::chrono::steady_clock::time_point));
-}
-
-void AMFITRACK::getSensorTimestamp(uint8_t DeviceID, std::chrono::steady_clock::time_point *time_stamp)
-{
-	if (!getDeviceActive(DeviceID))
-		return;
-#ifdef USE_THREAD_BASED
-	const std::lock_guard<std::mutex> lock(mutSensorTimestamps);
-#endif // USE_THREAD_BASED
-	memcpy(time_stamp, &SensorTimestamps[DeviceID], sizeof(std::chrono::steady_clock::time_point));
-}
-
 #endif
--- Amfitrack.h
+++ Amfitrack.h
@@ -7,31 +7,20 @@
 //-----------------------------------------------------------------------------
 
 #pragma once
-
-#include "AmfitrackDeviceTypes.h"
 #ifdef __cplusplus
 
 //-----------------------------------------------------------------------------
 // Section: Includes
 //-----------------------------------------------------------------------------
+#include "src/Amfitrack_Sensor.h"
+#include "src/Amfitrack_Source.h"
 
-#include "src/Amfitrack_Devices.h"
+#include "lib_AmfiProt_API.hpp"
 
-#include <array>
-#include <cstddef>
 #include <cstdint>
-
-#ifdef USE_THREAD_BASED
-#include <mutex>
-#endif
-
 //-----------------------------------------------------------------------------
 // Section: Define
 //-----------------------------------------------------------------------------
-
-#define AMFITRACK_NEW_DEVICE_COUNT 255U
-#define AMFITRACK_NEW_BROADCAST_DEVICE_ID 255U
-
 //-----------------------------------------------------------------------------
 // Section: Typedef
 //-----------------------------------------------------------------------------
@@ -47,7 +36,6 @@
 //-----------------------------------------------------------------------------
 // Section: Class
 //-----------------------------------------------------------------------------
-
 class AMFITRACK_NEW
 {
   public:
@@ -58,65 +46,40 @@ class AMFITRACK_NEW
 	void stop_task();
 	void run();
 
-	void reset_devices();
-	static bool is_valid_device_id(uint8_t device_id);
-	static std::size_t device_count();
-
-	//-----------------------------------------------------------------------------
-	// General
-	//-----------------------------------------------------------------------------
-	bool set(uint8_t device_id, bool isActive);
-	bool set(uint8_t device_id, char *name, uint8_t length);
-
-	//-----------------------------------------------------------------------------
-	// Sensor
-	//-----------------------------------------------------------------------------
 	bool get_sensor(uint8_t device_id, AMFITRACK_Sensor *sensor) const;
-	bool reset_sensor(uint8_t device_id);
-
-	bool set(uint8_t device_id, Pose_t const &pose);
-	bool set(uint8_t device_id, IMU_t const &imu);
-#if defined(_WIN32) || defined(__linux__) || defined(__APPLE__)
-	bool set(uint8_t device_id, std::chrono::steady_clock::time_point timestamp);
-#endif
-
-	//-----------------------------------------------------------------------------
-	// Source
-	//-----------------------------------------------------------------------------
 	bool get_source(uint8_t device_id, AMFITRACK_Source *source) const;
+
+	void reset_devices();
+	bool reset_sensor(uint8_t device_id);
 	bool reset_source(uint8_t device_id);
-	bool set(uint8_t device_id, Current_t const &current);
-	bool set(uint8_t device_id, Frequency_t const &frequency);
-	bool set(uint8_t device_id, Voltage_t const &voltage);
-	bool set(uint8_t device_id, Calibration_t const &calibration);
 
 	//-----------------------------------------------------------------------------
-	// Helper
+	// Old function will be deprecated
 	//-----------------------------------------------------------------------------
-	uint32_t get_time_ms()
-	{
-		return static_cast<uint32_t>(
-			std::chrono::duration_cast<std::chrono::milliseconds>(
-				std::chrono::steady_clock::now().time_since_epoch())
-				.count());
-	}
+  public:
+	void initialize_amfitrack();
+	void start_amfitrack_task(void);
+	void stop_amfitrack_task(void);
+	void amfitrack_main_loop(void);
+
+	bool getDeviceActive(uint8_t DeviceID);
+	void getDevicePose(uint8_t DeviceID, lib_AmfiProt_Amfitrack_Pose_t *Pose);
+	void getDeviceIMU(uint8_t DeviceID, lib_AmfiProt_Amfitrack_IMU_t *imuData);
+	void getSensorMeasurements(uint8_t DeviceID, lib_AmfiProt_Amfitrack_Sensor_Measurement_t *SensorMeasurement);
+#if defined(_WIN32) || defined(__linux__) || defined(__APPLE__)
+	void getSensorTimestamp(uint8_t DeviceID, std::chrono::steady_clock::time_point *time_stamp);
+#endif
+	//-----------------------------------------------------------------------------
+	//-----------------------------------------------------------------------------
 
   private:
-	AMFITRACK_NEW();
+	AMFITRACK_NEW() = default;
 	~AMFITRACK_NEW() = default;
 
 	AMFITRACK_NEW(AMFITRACK_NEW const &) = delete;
 	AMFITRACK_NEW &operator=(AMFITRACK_NEW const &) = delete;
 
 	static void background_amfitrack_task(AMFITRACK_NEW *);
-	void setup_device_slots();
-
-#ifdef USE_THREAD_BASED
-	mutable std::mutex _mutex;
-#endif
-
-	std::array<AMFITRACK_Sensor, AMFITRACK_NEW_DEVICE_COUNT> _sensors;
-	std::array<AMFITRACK_Source, AMFITRACK_NEW_DEVICE_COUNT> _sources;
 };
 
 #endif
